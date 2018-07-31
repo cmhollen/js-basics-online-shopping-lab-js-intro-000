a114082e6ec4f3b15ca0c84df9c3fe703ac0f088
cmhollen <cmhollenkamp@gmail.com>
2018-07-31 00:36:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,12 +10,11 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- var cart = [];
-  var price = Math.floor((Math.random()) * 100));
- var obj = {[item]: price};
- cart.push(obj);
-console.log(`${item} has been added to your cart.`);
- return cart;
+var cart = [];
+  var price = Math.floor(Math.random()*99)
+ cart.push(new Object({[item]:price}))
+ console.log(`${item} has been added to your cart.`)
+ return cart
 }
 
 function viewCart() {
